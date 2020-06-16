ae488cf48444da55d3410303e9a05e35ee3168b5
Chris <chrisoxygen1234@gmail.com>
2020-06-16 11:02:27 +0100

    added user profile feeds  api

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 
@@ -62,5 +63,20 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     
     def __str__(self):
         return self.email
+    
+    
+    
+    
+class ProfileFeedItem(models.Model):
+    """ profile status update """
+    user_profile = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        """ return the model as a string """
+        return self.status_text
+    
     
 
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,8 +4,11 @@ from rest_framework import status, viewsets, filters
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 
 from profiles_api import serializers, models, permissions
+
 
 
 
@@ -125,3 +128,19 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
     """ Handle create user authentication tokens """
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    
+    
+    
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        #IsAuthenticatedOrReadOnly
+        IsAuthenticated
+    )
+    
+    def perform_create(self, serializer):
+        """ set the user profile to the logged in user  """
+        serializer.save(user_profile=self.request.user)
